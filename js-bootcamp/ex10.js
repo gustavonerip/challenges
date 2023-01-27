3ebb35a1bdfc6c9ee090d073fd5f05b34f142724
Gustavo <gustavoneripeixoto@gmail.com>
2023-01-27 11:32:49 -0800

    Last 3 questions solved

---
+++ js-bootcamp/ex10.js
@@ -0,0 +1,15 @@
+/*
+Rewrite the following while loop as a for loop:
+let x = 9;
+while (x >= 1) {
+  console.log("hello " + x);
+  x = x - 1;
+}
+*/
+
+let x = 9;
+
+for(i = 0; x >= 1; i++){
+    console.log("hello " + x);
+    x -= 1;
+}
